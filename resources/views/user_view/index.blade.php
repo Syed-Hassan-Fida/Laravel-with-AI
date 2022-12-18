dab1efaa4ca85b76f7b0667ba198f6b2cd6f31a2
Syed-Hassan-Fida <hassansfida@gmail.com>
2022-12-18 16:52:50 +0500

    home page updated

--- resources/views/user_view/index.blade.php
+++ resources/views/user_view/index.blade.php
@@ -15,6 +15,12 @@
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="{{asset('assets2\css\styles.css')}}" rel="stylesheet" />
         {{-- <link href="{{asset('assets\scss\my_scss.scss')}}" rel="stylesheet" /> --}}
+        {{-- jquery cdn --}}
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+        {{-- fontawesom --}}
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"></script>
+
 
     </head>
     <body id="page-top">
@@ -75,18 +81,242 @@
                 </div>
             </div>
         </header>
+
         <div class="container">
-            <h1>Welcome</h1>
+
+            <div class="container m-4 mb-5">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card p-3 mb-2">
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row align-items-center">
+                                    <div class="icon"> <i class="fa-solid fa-blog"></i> </div>
+                                    <div class="ms-2 c-details">
+                                        <h6 class="mb-0">Blog Intro Generation</h6> <span>Rating: 4.6</span>
+                                    </div>
+                                </div>
+                                <div class="badge"> <a href="" class="btn"><span>AI Tool</span></a> </div>
+                            </div>
+                            <div class="mt-5">
+                                <h3 class="heading">AI<br>BLOG-Generation</h3>
+                                <div class="mt-5">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                    <div class="mt-3"> <span class="text1">32 Users <span class="text2">Applied</span></span> </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card p-3 mb-2">
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row align-items-center">
+                                    <div class="icon"> <i class="fa-solid fa-blog"></i> </div>
+                                    <div class="ms-2 c-details">
+                                        <h6 class="mb-0">Blog Intro Generation</h6> <span>Rating: 4.6</span>
+                                    </div>
+                                </div>
+                                <div class="badge"> <a href="" class="btn"><span>AI Tool</span></a> </div>
+                            </div>
+                            <div class="mt-5">
+                                <h3 class="heading">AI<br>BLOG-Generation</h3>
+                                <div class="mt-5">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                    <div class="mt-3"> <span class="text1">32 Users <span class="text2">Applied</span></span> </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card p-3 mb-2">
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row align-items-center">
+                                    <div class="icon"> <i class="fa-solid fa-blog"></i> </div>
+                                    <div class="ms-2 c-details">
+                                        <h6 class="mb-0">Blog Intro Generation</h6> <span>Rating: 4.6</span>
+                                    </div>
+                                </div>
+                                <div class="badge"> <a href="" class="btn"><span>AI Tool</span></a> </div>
+                            </div>
+                            <div class="mt-5">
+                                <h3 class="heading">AI<br>BLOG-Generation</h3>
+                                <div class="mt-5">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                    <div class="mt-3"> <span class="text1">32 Users <span class="text2">Applied</span></span> </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container m-4 mb-5">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card p-3 mb-2">
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row align-items-center">
+                                    <div class="icon"> <i class="fa-solid fa-blog"></i> </div>
+                                    <div class="ms-2 c-details">
+                                        <h6 class="mb-0">Blog Body Generation</h6> <span>Rating: 4.6</span>
+                                    </div>
+                                </div>
+                                <div class="badge"> <a href="" class="btn"><span>AI Tool</span></a> </div>
+                            </div>
+                            <div class="mt-5">
+                                <h3 class="heading">AI<br>BLOG-Generation</h3>
+                                <div class="mt-5">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                    <div class="mt-3"> <span class="text1">32 Users <span class="text2">Applied</span></span> </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card p-3 mb-2">
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row align-items-center">
+                                    <div class="icon"> <i class="fa-solid fa-blog"></i> </div>
+                                    <div class="ms-2 c-details">
+                                        <h6 class="mb-0">Blog Body Generation</h6> <span>Rating: 4.6</span>
+                                    </div>
+                                </div>
+                                <div class="badge"> <a href="" class="btn"><span>AI Tool</span></a> </div>
+                            </div>
+                            <div class="mt-5">
+                                <h3 class="heading">AI<br>BLOG-Generation</h3>
+                                <div class="mt-5">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                    <div class="mt-3"> <span class="text1">32 Users <span class="text2">Applied</span></span> </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card p-3 mb-2">
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row align-items-center">
+                                    <div class="icon"> <i class="fa-solid fa-blog"></i> </div>
+                                    <div class="ms-2 c-details">
+                                        <h6 class="mb-0">Blog Body Generation</h6> <span>Rating: 4.6</span>
+                                    </div>
+                                </div>
+                                <div class="badge"> <a href="" class="btn"><span>AI Tool</span></a> </div>
+                            </div>
+                            <div class="mt-5">
+                                <h3 class="heading">AI<br>BLOG-Generation</h3>
+                                <div class="mt-5">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                    <div class="mt-3"> <span class="text1">32 Users <span class="text2">Applied</span></span> </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
 
+
         <!-- Footer-->
-        <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; Your Website 2022</div></footer>
+        <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; By Source Code Solutions 2022</div></footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
         <script src="js/scripts.js"></script>
 
         <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+
+        <style>
+            body {
+                background-color: #eee
+            }
+
+            .card {
+                border: none;
+                border-radius: 10px
+            }
+
+            .c-details span {
+                font-weight: 300;
+                font-size: 13px
+            }
+
+            .icon {
+                width: 50px;
+                height: 50px;
+                background-color: #eee;
+                border-radius: 15px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                font-size: 39px
+            }
+
+            .badge span {
+                background-color: #fffbec;
+                width: 60px;
+                height: 25px;
+                padding-bottom: 3px;
+                border-radius: 5px;
+                display: flex;
+                color: #f1bb09be;
+                justify-content: center;
+                align-items: center
+            }
+
+            .progress {
+                height: 10px;
+                border-radius: 10px
+            }
+
+            .progress div {
+                background-color: red
+            }
+
+            .text1 {
+                font-size: 14px;
+                font-weight: 600
+            }
+
+            .text2 {
+                color: #a5aec0
+            }
+            .colored{
+                /* background-color: #fff !important; */
+                /* transition: background-color 200ms linear; */
+                color: black;
+            }
+        </style>
+
+        <script>
+            $(document).ready(function(){
+            $(window).scroll(function(){
+                var scroll = $(window).scrollTop();
+                console.log(scroll)
+                if (scroll > 540) {
+                    $(".nav-link").css("color", "black");
+                    $(".navbar-brand").css("color", "black");
+                } else {
+                    $(".nav-link").css("color", "rgba(255, 255, 255, 0.5)");
+                    $(".navbar-brand").css("color", "rgba(255, 255, 255, 0.5)");
+                }
+
+
+            })
+        })
+        </script>
     </body>
 </html>
